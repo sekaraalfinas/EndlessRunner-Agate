cd1fc0566f0f081a0bd32fe29009416ed55a6832
Sekar Ayu Alfina Shasty <sekar.ayu.1803316@students.um.ac.id>
2021-09-15 18:42:01 +0700

    Tugas 3 Fase Pengetahuan Per Bidang (Game Programming) oleh Sekar Ayu Alfina Shasty, ID Agate: 49

---
+++ 2D Endless Runner/Assets/Scripts/CharacterSoundController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundController : MonoBehaviour
+{
+    public AudioClip jump;
+
+    public AudioClip scoreHighlight;
+
+    private AudioSource audioPlayer;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    public void playJump()
+    {
+        audioPlayer.PlayOneShot(jump);
+    }
+
+    public void PlayScoreHighlight()
+    {
+        audioPlayer.PlayOneShot(scoreHighlight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2D Endless Runner/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    [Header("ScoreHighlight")]
+    public int scoreHighlightRange;
+    public CharacterSoundController sound;
+
+    public int lastScoreHighlight = 0;
+
+    private int currentScore = 0;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        // reset
+        currentScore = 0;
+        lastScoreHighlight = 0;
+    }
+
+    public float GetCurrentScore()
+    {
+        return currentScore;
+    }
+
+    public void IncreaseCurrentScore(int increment)
+    {
+        currentScore += increment;
+
+        if(currentScore - lastScoreHighlight > scoreHighlightRange)
+        {
+            sound.PlayScoreHighlight();
+            lastScoreHighlight += scoreHighlightRange;
+        }
+    }
+
+    public void FinishScoring()
+    {
+        // set high score
+        if (currentScore > ScoreData.highScore)
+        {
+            ScoreData.highScore = currentScore;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
